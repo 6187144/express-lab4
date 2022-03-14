bec9f0035d29d439704b11edc74e4598c5faffa1
6187144 <6187144@cegep-heritage.qc.ca>
2022-03-14 10:24:51 -0400

    added menu model and view

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,4 +43,4 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+module.exports = app; 
